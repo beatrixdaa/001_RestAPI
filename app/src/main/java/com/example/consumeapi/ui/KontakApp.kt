17cda4d6ddb78ef8411edae5edc22d073650683a
beatrixdaa <beatrixdevanti21@gmail.com>
2023-12-25 22:31:50 +0700

    revisi

--- app/src/main/java/com/example/consumeapi/ui/KontakApp.kt
+++ app/src/main/java/com/example/consumeapi/ui/KontakApp.kt
@@ -1,9 +1,14 @@
 package com.example.consumeapi.ui
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -46,12 +51,26 @@ Surface  (
 @Composable
 fun TopAppBarKontak(
     title : String,
-    canNavigateBack : Modifier  = Modifier,
+    canNavigateBack : Boolean,
+    modifier : Modifier  = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
 ){
-    
-}
+    CenterAlignedTopAppBar(title ={ Text (title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = ""
+                    )
+                }
+            }
+        })
+    }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
