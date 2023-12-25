13faa90f41a712bcab32095c1b043603fcf1b294
beatrixdaa <beatrixdevanti21@gmail.com>
2023-12-25 23:40:40 +0700

    add InsertVscreen

---
+++ app/src/main/java/com/example/consumeapi/ui/kontak/screen/InsertScreen.kt
@@ -0,0 +1,20 @@
+package com.example.consumeapi.ui.kontak.screen
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.consumeapi.ui.kontak.viewmodel.InsertUiEvent
+import com.example.consumeapi.ui.kontak.viewmodel.InsertUiState
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier : Modifier = Modifier,
+    onValueChange : (InsertUiEvent) -> Unit = {},
+    enabled : Boolean = true
+){
+    Column (
+
+    )
+}
