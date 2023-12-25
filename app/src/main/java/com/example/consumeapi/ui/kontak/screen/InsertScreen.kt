6f1885b4b1216e5bdde262765ed67df91194b1fd
beatrixdaa <beatrixdevanti21@gmail.com>
2023-12-25 23:41:02 +0700

    add InsertVscreen

--- app/src/main/java/com/example/consumeapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/kontak/screen/InsertScreen.kt
@@ -1,5 +1,6 @@
 package com.example.consumeapi.ui.kontak.screen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
